b1b38f8d1547af248fa1153470c3822c035ee520
Alelewi, Almokhtar <almokhtar.alelewi@congstar.net>
2019-05-26 16:11:41 +0200

    Erstes Einchecken des Moduls Game

---
+++ Game/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+
+/*
+ * diese Klasse für die Implemntierung von Player und Inthaltet :
+ * der zeichnen von der  Board wo der Spieler darstehen wird .
+ * der jeztige Position von der Spieler
+ * schiessen
+ * und bewegen 
+ * **/
+namespace Game
+{
+    class Player
+    {
+
+        private int width = 0;
+        private int height = 0;
+        private int s = 0;
+        //zeichnen von Board des spiel 
+        public void DrawBoard(String[,] board)
+        {
+            width = board.GetUpperBound(0);
+            height = board.GetUpperBound(1);
+            Console.SetWindowSize(width + 5, height + 5);
+            for (int i = 0; i < board.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < board.GetUpperBound(1); j++)
+                {
+                    if (i == 0 || j == 0 || i == board.GetUpperBound(0) - 1 || j == board.GetUpperBound(1) - 1)
+                    {
+                        board[i, j] = "#";
+                    }
+                    else
+                    {
+                        board[i, j] = " ";
+                    }
+                    board = DrawPlayer(board, s);
+                    Console.Write(board[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+        private void getPlayerPostion()
+        {
+
+        }
+        // zeichnen von spieler 
+        public String[,] DrawPlayer(String[,] board,int i = 0)
+        {
+            board[board.GetUpperBound(0) - 2, (board.GetUpperBound(1) / 2) - 1 + i] = "@";
+            board[board.GetUpperBound(0) - 2, (board.GetUpperBound(1) / 2)+ i] = "@";
+            board[board.GetUpperBound(0) - 2, (board.GetUpperBound(1) / 2) +1 + i] = "@";
+            board[board.GetUpperBound(0) - 3, (board.GetUpperBound(1) / 2)+ i] = "@";
+            return board;
+        }
+        //bewegen 
+        public void Move(Program.Dirction dirction, string[,] board)
+        {
+            //wenn die Dirction Left wird der Player auf 4 plätze links verschoben
+            if (dirction == Program.Dirction.Left)
+            {
+                Console.Clear();
+                    s = s + (-3);
+                    DrawBoard(board);
+             
+
+            }
+            if(dirction == Program.Dirction.Right)
+            {
+                Console.Clear();
+                    s = s + 3;
+                    DrawBoard(board);
+                
+               
+            }
+          
+
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        // der enum Dirction enthalt möglich bewegung richtungen wird später beim logik von bewegung benutzt
+        /**
+         *z.B:
+         *if (key == 'A'){
+         *  player.move(Dirction.Left)
+         * }
+         */
+        public enum Dirction
+        {
+            Left,
+            Right
+        }
+        static void Main(string[] args)
+        {
+            //initalziern von der Große des Spielfelds
+            int width = 40;
+            int height = 40;
+            String[,] board = new String[height + 1, width + 1];
+            //initalzierung von Player als Objekt 
+            Player player = new Player();
+            //zeichnen von Spielfelds 
+            //aufrufen von DrawBoard Methode
+            player.DrawBoard(board);
+
+            // test 
+            //bewegen von  der spieler nach links 
+            while (true)
+            {
+                ConsoleKey userInput = Console.ReadKey().Key;
+                if (userInput == ConsoleKey.RightArrow)
+                {
+                    player.Move(Dirction.Right, board);
+                }
+                else if (userInput == ConsoleKey.LeftArrow)
+                {
+                    player.Move(Dirction.Left, board);
+                }
+            }
+          
+            Console.ReadKey();
+        }
+    }
+}
